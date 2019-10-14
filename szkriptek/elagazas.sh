2ae6766aab4a0340e2dc64a7d092ea9228de581d
pinterp117 <pinterp117@gmail.com>
2019-10-14 07:43:32 +0000

    elagazas.sh

---
+++ szkriptek/elagazas.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo -n "Adjon meg egy szamot: "
+read szam;
+if [ $szam -lt 5 ]; then
+echo "A szam kisebb mint 5."
+elif [ $szam -gt 5 ]; then
+echo "A szam nagyobb mint 5."
+else
+echo "A szam pontosan 5."
+fi
+exit 0
