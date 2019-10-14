7e5e848b784b7eaa9b4a550ffbceba4cf1c8fdd7
pinterp117 <pinterp117@gmail.com>
2019-10-14 07:09:17 +0000

    hello script letrehozasa.

---
+++ szkriptek/hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Ez egy megjegyzés. Megjegyzéseket a '#' jellel tudunk a kódba írni. Ezeket a fordító nem veszi figyelembe
+echo "Hello World!"
+exit 0 # az exit 0 mindig a helyes programfutás utáni kilépést jelenti.
